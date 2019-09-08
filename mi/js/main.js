a1ed60ca670235cf5db84037f667af4653e1218a
Mikeccx <545860057@qq.com>
2019-09-08 19:15:24 +0800

    addexplatation

---
+++ mi/js/main.js
@@ -0,0 +1,32 @@
+function hide(e) {
+    e.firstElementChild.style.display = "none"
+}
+function display(e) {
+    e.firstElementChild.style.display = "block"
+}
+function hidecart(e) {
+    e.children[1].style.display = "none"
+}
+function displaycart(e) {
+    e.children[1].style.display = "block"
+}
+window.onscroll = function (e) {
+    let elementop = document.getElementsByClassName('content')[0].offsetTop  //该元素和页面顶端的距离
+    //  console.log(document.getElementsByClassName('content-top-center')[0].offsetTop)
+    //  console.log(document.getElementsByTagName('body')[0])
+    let scrolltop = document.documentElement.scrollTop || document.body.scrollTop  //滚动距离
+    let banner = document.getElementsByClassName('content-top-center')[0]
+    console.log(banner.className);
+    // console.log(document.documentElement.scrollTop || document.body.scrollTop)
+    if (scrolltop - elementop >= 0) {
+        console.log('fixed')
+        banner.className = "content-top-center fixed"  //修改样式
+    }
+    else {
+        console.log('relative')
+        banner.className = "content-top-center relative"  //修改
+
+
+    }
+
+}
